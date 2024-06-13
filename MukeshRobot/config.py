b888d29668d54d3ceeadac6e2f261f7d3ed2d964
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-06-13 22:36:58 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,35 +1,30 @@
 
 class Config(object):
     LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
+ 
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID =27383453
+    API_HASH =4c246fb0c649477cc2e79b6a178ddfaa
+    TOKEN =7394970820:AAF7_pWYaJ__LkL2PQ7dA7BhlbVvdsZUvoQ
+    OWNER_ID =6352107773
     
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
-    START_IMG = ""
+    SUPPORT_CHAT =HEROKUBIN_01
+    START_IMG =https://te.legra.ph/file/386fdbaf3f9a740f129da.jpg
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
-    # RECOMMENDED
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
-    CASH_API_KEY = (
-        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
-    )
-    TIME_API_KEY = ""
+    MONGO_DB_URI=mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/
     
-    # Get your API key from https://timezonedb.com/api
-
+BOT_USERNAME =Test_robo_test_bot
+    DATABASE_URL =postgres://iarfggbc:Vxzh_kG7cxa1kHR5faxcd1kuA4R-UT9E@rosie.db.elephantsql.com/iarfggbc
+    CASH_API_KEY =hbnb
+    TIME_API_KEY =nej
 
     # Optional fields
-    BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    BL_CHATS = [6898413162]  # List of groups that you want blacklisted.
+    DRAGONS = [6898413162]  # User id of sudo users
+    DEV_USERS = [6898413162]  # User id of dev users
+    DEMONS = [6898413162]  # User id of support users
+    TIGERS = [6898413162]  # User id of tiger users
+    WOLVES = [6898413162]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
@@ -48,3 +43,4 @@ class Production(Config):
 
 class Development(Config):
     LOGGER = True
+    
